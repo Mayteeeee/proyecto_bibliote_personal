0e9040c5e31e5fe31224d24aa342c8116884f036
Mayteee <cecicanseco328@gmail.com>
2026-05-27 19:22:55 -0600

    avance

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Aquí abajo le agregamos el ".component" a la ruta del archivo
+import { Recuperacion } from './pantallas/recuperacion/recuperacion'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Recuperacion], // Ahora sí, esto funcionará perfecto
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
